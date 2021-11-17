e1d7fe5d9a06880d1b413f5c78b5a20af18ed4d7
coldplay126 <coldplay126@gmail.com>
2021-11-17 19:21:14 +0900

    :art: login and logout
    로그인, 로그아웃 기능 생성

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,18 +1,24 @@
 import { Route, Routes } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
-const AppRouter = ({ isLogIn, auth }) => {
+const AppRouter = ({ isLogIn }) => {
   return (
-    <Routes>
-      {isLogIn ? (
-        <>
-          <Route exact path="/" element={<Home />} />
-        </>
-      ) : (
-        <Route exact path="/" element={<Auth auth={auth} />} />
-      )}
-    </Routes>
+    <>
+      <div>{isLogIn && <Navigation />}</div>
+      <Routes>
+        {isLogIn ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </>
+        ) : (
+          <Route path="/" element={<Auth />} />
+        )}
+      </Routes>
+    </>
   );
 };
 
