48995ec10ae766c558078d9713474b2c832538bc
coldplay126 <coldplay126@gmail.com>
2021-12-27 01:13:45 +0900

    🚀 첫 디플로이

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,41 +3,26 @@ import Auth from "routes/Auth";
 import Home from "routes/Home";
 import Profile from "routes/Profile";
 import Navigation from "components/Navigation";
+import { BrowserRouter } from "react-router-dom";
 
-const AppRouter = ({
-  isLogIn,
-  userObj,
-  refreshUser,
-}) => {
+const AppRouter = ({ isLogIn, userObj, refreshUser }) => {
   return (
-    <>
-      <div>
-        {isLogIn && (
-          <Navigation userObj={userObj} />
-        )}
-      </div>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div>{isLogIn && <Navigation userObj={userObj} />}</div>
       <Routes>
         {isLogIn ? (
           <>
-            <Route
-              path="/"
-              element={<Home userObj={userObj} />}
-            />
+            <Route path="/" element={<Home userObj={userObj} />} />
             <Route
               path="/profile"
-              element={
-                <Profile
-                  refreshUser={refreshUser}
-                  userObj={userObj}
-                />
-              }
+              element={<Profile refreshUser={refreshUser} userObj={userObj} />}
             />
           </>
         ) : (
           <Route path="/" element={<Auth />} />
         )}
       </Routes>
-    </>
+    </BrowserRouter>
   );
 };
 
